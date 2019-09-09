225bd70c975830ff173ba6223f52c17304acae18
zhoujie <2083491778@qq.com>
2019-09-09 20:09:27 +0800

    test

--- src/main/java/com/sy/config/SpringBootStarter.java
+++ src/main/java/com/sy/config/SpringBootStarter.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class SpringBootStarter {
 
     public static void main(String[] args) {
+        System.out.println("gegsgsegseg");
         SpringApplication.run(SpringConfig.class,args);
     }
 }
